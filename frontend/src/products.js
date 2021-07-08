292e6110b404ce214409864e626eabaf25001b4e
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-08 00:37:36 -0400

    Create products.js

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'jsnkdvlknsd',
+    image: '/images/airpods.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    _id: '2',
+    name: 'jsnkdvlknsd',
+    image: '/images/alexa.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    _id: '3',
+    name: 'jsnkdvlknsd',
+    image: '/images/airpods.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    _id: '4',
+    name: 'jsnkdvlknsd',
+    image: '/images/airpods.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    _id: '5',
+    name: 'jsnkdvlknsd',
+    image: '/images/airpods.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    _id: '6',
+    name: 'jsnkdvlknsd',
+    image: '/images/airpods.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+];
+
+export default products;

02eca5c17467aba9268fc06b7c05eb92058091fb
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-12 19:28:44 -0400

    Update products.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,53 @@
 const products = [
   {
-    _id: '1',
+    name: 'The Gaming Chair',
+    image: '/images/gaming_chair.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'PlayStation 5',
+    image: '/images/ps5.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'jsnkdvlknsd',
+    image: '/images/gaming_mouse.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'jsnkdvlknsd',
+    image: '/images/gaming_keyboard.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
     name: 'jsnkdvlknsd',
     image: '/images/airpods.jpg',
     description:
@@ -13,59 +60,6 @@ const products = [
     numReviews: 9,
   },
   {
-    _id: '2',
-    name: 'jsnkdvlknsd',
-    image: '/images/alexa.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    _id: '3',
-    name: 'jsnkdvlknsd',
-    image: '/images/airpods.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    _id: '4',
-    name: 'jsnkdvlknsd',
-    image: '/images/airpods.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    _id: '5',
-    name: 'jsnkdvlknsd',
-    image: '/images/airpods.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    _id: '6',
     name: 'jsnkdvlknsd',
     image: '/images/airpods.jpg',
     description:

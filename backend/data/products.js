80aa6cb16de62b8b630d7f44bc74f54bc4fecc95
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-13 20:41:03 -0400

    Update products.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,54 +1,6 @@
 const products = [
   {
-    name: 'The Gaming Chair',
-    image: '/images/gaming_chair.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    name: 'PlayStation 5',
-    image: '/images/ps5.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    name: 'jsnkdvlknsd',
-    image: '/images/gaming_mouse.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    name: 'jsnkdvlknsd',
-    image: '/images/gaming_keyboard.jpg',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
-    brand: 'efwef',
-    category: 'wefewfewf',
-    price: 99.99,
-    countInStock: 5,
-    rating: 4.9,
-    numReviews: 9,
-  },
-  {
-    name: 'jsnkdvlknsd',
+    name: 'Apple Airpods',
     image: '/images/airpods.jpg',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
@@ -60,8 +12,56 @@ const products = [
     numReviews: 9,
   },
   {
-    name: 'jsnkdvlknsd',
-    image: '/images/airpods.jpg',
+    name: 'Amazon Alexa',
+    image: '/images/alexa.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'Apple iPhone',
+    image: '/images/phone.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
+    brand: 'efwef',
+    category: 'wefewfewf',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 9,
+  },
+  {
+    name: 'PlayStation 4',
+    image: '/images/playstation.jpg',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates a totam beatae culpa quae deleniti? Necessitatibus, commodi magni maxime doloremque accusamus maiores sunt tenetur molestiae. Tenetur vitae ducimus ex!',
     brand: 'efwef',

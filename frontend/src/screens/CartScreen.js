973e5ada095fe6565d1979cc045da97b3e621013
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-16 21:46:05 -0400

    Update CartScreen.js

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -13,8 +13,12 @@ import {
 import Message from '../components/Message';
 import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
-  return <div>cart</div>;
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  return <div>Cart</div>;
 };
 
 export default CartScreen;

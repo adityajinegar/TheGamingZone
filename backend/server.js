16fec8884b4f5c1079f9f6695355a734ab588209
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-17 16:11:12 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -11,6 +12,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running...');

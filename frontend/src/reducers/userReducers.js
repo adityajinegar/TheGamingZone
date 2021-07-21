eae87139364a8e4208861160b382ef8229a1bfa2
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-20 20:23:56 -0400

    Update userReducers.js

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -50,6 +50,9 @@ export const userRegisterReducer = (state = {}, action) => {
         error: action.payload,
       };
 
+    case USER_LOGOUT:
+      return {};
+
     default:
       return state;
   }

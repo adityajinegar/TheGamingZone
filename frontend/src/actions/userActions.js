0aa78e78306b9838d7fcb6fb056acda2ebb06ce6
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-23 02:21:39 -0400

    Update userActions.js

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -109,7 +109,6 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
@@ -152,6 +151,13 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,

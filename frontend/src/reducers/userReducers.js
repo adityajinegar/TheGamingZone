a7b6a8a96e77a1d71180a86691946b35d0ef2294
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-23 19:49:43 -0400

    Update userReducers.js

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -110,6 +113,30 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       };
 
     case USER_UPDATE_PROFILE_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const userListReducer = (state = { user: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case USER_LIST_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+      };
+
+    case USER_LIST_FAIL:
       return {
         loading: false,
         error: action.payload,

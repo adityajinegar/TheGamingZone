37e4d4095de77dbd0ff9e86e1d6eaadfcfa28f41
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-23 19:00:20 -0400

    Update Rating.js

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -71,5 +71,11 @@ const Rating = ({ value, text, color }) => {
 Rating.defaultProps = {
   color: '#f8e825',
 };
+
+// Rating.propTypes = {
+//   value: PropTypes.number.isRequired,
+//   text: PropTypes.string.isRequired,
+//   color: PropTypes.string,
+// };
 
 export default Rating;

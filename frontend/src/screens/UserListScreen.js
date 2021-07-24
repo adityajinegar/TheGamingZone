7e83bc5cd27e445da57f930f14ee1e1c2443a963
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-24 00:38:26 -0400

    Update UserListScreen.js

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -27,7 +27,9 @@ const UserListScreen = ({ history }) => {
   }, [dispatch, userInfo, history, successDelete]);
 
   const deleteHandler = (id) => {
-    dispatch(deleteUser(id));
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (

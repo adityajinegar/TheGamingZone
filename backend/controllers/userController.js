6a8043b0fcbff2ac754d6f58604d893147b7e059
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-24 00:51:35 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc   Update user proofile
+// @desc   Update user profile
 // @route  PUT /api/users/profile
 // @access Private
 
@@ -135,6 +135,46 @@ const deletUser = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+// @desc   Update user
+// @route  PUT /api/users/:id
+// @access Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+});
+
 export {
   authUser,
   registerUser,
@@ -142,4 +182,6 @@ export {
   updateUserProfile,
   getUsers,
   deletUser,
+  getUserById,
+  updateUser,
 };

a4da0db5eb7b8c0eb417674fdec9cae85aa29b33
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-23 20:08:13 -0400

    Create UserListScreen.js

---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listUsers } from '../actions/userActions';
+
+const UserListScreen = () => {
+  const dispatch = useDispatch();
+
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}></a>
+                  {user.email}
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <i className='fas fa-check' style={{ color: 'green' }}></i>
+                  ) : (
+                    <i className='fas fa-times' style={{ color: 'red' }}></i>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default UserListScreen;

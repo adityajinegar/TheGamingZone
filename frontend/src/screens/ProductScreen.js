2a5968877c5c67a0b65c7312d587e5c846ef29b9
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-25 21:18:49 -0400

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -26,7 +26,7 @@ const ProductScreen = ({ history, match }) => {
 
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
-  }, [dispatch, match.params.id]);
+  }, [dispatch, match]);
 
   const addToCartHandler = () => {
     history.push(`/cart/${match.params.id}?qty=${qty}`);

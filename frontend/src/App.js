2ac3be3f56011e1e43148b9dc203a9a022135137
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-25 22:31:04 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 const App = () => {
   return (
@@ -31,9 +32,10 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/admin/userlist' component={UserListScreen}></Route>
+          <Route path='/admin/user/:id/edit' component={UserEditScreen}></Route>
           <Route
-            path='/admin/user/:id/edit'
-            component={UserEditScreen}
+            path='/admin/productlist'
+            component={ProductListScreen}
           ></Route>
           <Route path='/' component={HomeScreen} exact></Route>
           {/* ? means id is optional */}
